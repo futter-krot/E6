9b0f293f2fc87af08e0d87d953d5dd89140f7506
futter-krot <66520330+futter-krot@users.noreply.github.com>
2021-08-05 21:40:53 +0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,14 +1,24 @@
-version: '3.7'
-services:
-    flask:
-        build: .
-        ports:
-            - "5000:5000"
-        environment:
-            - REDIS_HOST=redis
-        depends_on:
-            - redis
-    redis:
-        image: redis:5.0.7
-        ports:
-            - "6379:6379"
+from redis import Redis
+from flask import jsonify, Flask, request
+import os
+
+app = Flask(__name__)
+REDIS_HOST = os.environ.get('REDIS_HOST')
+redis_client = Redis(host=REDIS_HOST)
+
+@app.route("/<number>", methods=['GET'])
+def get_fibonacci_api(number):
+    number = int(number)
+    stored_value = redis_client.get(number)
+    if stored_value:
+        logger.info("For %s stored value is used" % number)
+        return jsonify({number: stored_value.decode()}), 200
+    new_value = get_fibonacci(number)
+    logger.info("For %s new value is calculated" % number)
+    redis_client.set(number, new_value)
+    return jsonify({number: new_value}), 200
+
+def get_fibonacci(number):
+    if (number == 0) or (number == 1): 
+        return number
+    return get_fibonacci(number-1) + get_fibonacci(number-2)
