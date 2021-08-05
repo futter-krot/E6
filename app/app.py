674297e8ac1e075ab8e592ee466209a43d40a556
futter-krot <66520330+futter-krot@users.noreply.github.com>
2021-08-05 21:39:48 +0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,22 +1,14 @@
-from pymemcache.client.base import Client
-from flask import jsonify, Flask, request
-import os
-
-app = Flask(__name__)
-memcache_client = Client(("localhost", 11211))
-
-
-@app.route('/<k>')
-def get_fibo_num(k):
-    fibo = memcache_client.get(k)
-    print(fibo)
-    if fibo is None:
-        fibo = count_fibo_num(int(k))        
-        memcache_client.set(k, fibo)
-    return jsonify(fibo=int(fibo)), 200
-
-def count_fibo_num(k):
-    fibo, fibo_next = 0, 1
-    for _ in range(k - 1):
-        fibo, fibo_next = fibo_next, fibo + fibo_next
-    return fibo
+version: '3.7'
+services:
+    flask:
+        build: .
+        ports:
+            - "5000:5000"
+        environment:
+            - REDIS_HOST=redis
+        depends_on:
+            - redis
+    redis:
+        image: redis:5.0.7
+        ports:
+            - "6379:6379"
